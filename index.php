8b9535d6ada1ea3f0a60deb321212c3a4b03de9c
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-03-10 14:25:08 +1100

    Added login form with tidy up

--- index.php
+++ index.php
@@ -1,52 +1,53 @@
-<?php echo "Hello World"; ?> 
- <!DOCTYPE html>
- <html lang="en">
-   <head>
-     <meta charset="utf-8" />
-     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-     <meta name="viewport" content="width=device-width, initial-scale=1" />
-     <meta name="robots" content="follow">
+<!DOCTYPE html>
+<html lang="en">
 
-     <title>Page Title</title>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="follow">
 
-     <base href="/" />
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
-   </head>
+    <title>Page Title</title>
 
-   <body>
+    <base href="/" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+</head>
 
-   	<div class="uk-section uk-container">
-   		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
- 			<form class="uk-form-stacked js-login">
+<body>
 
- 			    <div class="uk-margin">
- 			        <label class="uk-form-label" for="form-stacked-text">Email</label>
- 			        <div class="uk-form-controls">
- 			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
- 			        </div>
- 			    </div>
+    <div class="uk-section uk-container">
+        <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+            <form class="uk-form-stacked js-login">
 
- 			    <div class="uk-margin">
- 			        <label class="uk-form-label" for="form-stacked-text">Password</label>
- 			        <div class="uk-form-controls">
- 			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
- 			        </div>
- 			    </div>
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Email</label>
+                    <div class="uk-form-controls">
+                        <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+                    </div>
+                </div>
 
- 			    <div class="uk-margin">
- 			        <button class="uk-button uk-button-default" type="submit">Login</button>
- 			    </div>
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Password</label>
+                    <div class="uk-form-controls">
+                        <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
+                    </div>
+                </div>
 
- 			</form>
-   		</div>
-   	</div>
+                <div class="uk-margin">
+                    <button class="uk-button uk-button-default" type="submit">Login</button>
+                </div>
 
-     <!-- jQuery is required -->
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            </form>
+        </div>
+    </div>
 
-     <!-- UIkit JS -->
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+    <!-- jQuery is required -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-   </body>
- </html>
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+
+</body>
+
+</html>
