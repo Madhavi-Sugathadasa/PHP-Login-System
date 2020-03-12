2de7eb937fae28d3c0f1606da37f66e2ca451888
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-03-12 22:32:11 +1100

    Added registration via Ajax

--- inc/config.php
+++ inc/config.php
@@ -7,9 +7,15 @@
 
 
  	// Our config is below
-    
-    // Include the DB.php file;
+
+ 	// Allow errors
+ 	error_reporting(-1);
+ 	ini_set('display_errors', 'On');
+
+
+ 	// Include the DB.php file;
  	include_once "classes/DB.php";
+ 	include_once "classes/Filter.php";
 
-
+ 	$con = DB::getConnection();
  ?>
